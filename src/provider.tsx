5a74b04c94018d38c520ae4e7c978970dbe7462e
Diego López Ariza <diegoawesome2017@outlook.com>
2026-04-01 18:44:54 -0600

    Deleted unused provider

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,12 +1,9 @@
 import { MovieProvider } from "./features/movie";
-import { WebTorrentProvider } from "./features/webtorrent/application/providers/webTorrentProvider";
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <WebTorrentProvider>
-      <MovieProvider>
-        {children}
-      </MovieProvider>
-    </WebTorrentProvider>
+    <MovieProvider>
+      {children}
+    </MovieProvider>
   );
 }
